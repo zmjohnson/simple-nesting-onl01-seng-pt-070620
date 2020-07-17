9d5eaefe4e419e79348f6c5d9933358776eca27f
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-17 12:52:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
      
 end
 
