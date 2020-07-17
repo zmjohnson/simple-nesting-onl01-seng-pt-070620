dd8c15c622c5d377b4e2bb938f6f124891b113e0
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-17 12:50:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash.merge(:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP, C"]
+ })
     
 end
 
